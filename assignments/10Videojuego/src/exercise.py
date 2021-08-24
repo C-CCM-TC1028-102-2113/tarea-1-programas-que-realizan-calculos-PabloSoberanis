70ae29df521c54051196c0a49d7d0feebf01fa3d
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-08-23 23:14:44 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-   pass
+    n = int(1000)
+    u = int(350)
+
+    nc = int(input("Dame la cantidad de juegos nuevos:"))
+    uc = int(input("Dame la cantidad de juegos usados:"))
+
+    comp = nc * n + uc * u
+
+    print("El total de la compra es:", comp)
+
+    pass
+   
 
 
 
